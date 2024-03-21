0d8f53ee778cd841838a65de860ac1398296b727
Ezequiel Baravalle <ezebaravalle@gmail.com>
2024-03-21 00:06:22 -0300

    V6

--- D365ConsoleApp/Program.cs
+++ D365ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Aplicación_de_consola_D365
+﻿using System;
+
+namespace Aplicación_de_consola_D365
 {
     class Program
     {
@@ -6,6 +8,25 @@
         {
 
             System.Console.WriteLine("Hello");
+
+            string connectionString = $@"";
+
+            using (var svc = new CrmServiceClient(connectionString))
+            {
+
+                if (svc.IsReady)
+                {
+                    WhoAmIRequest request = new WhoAmIRequest();
+
+                    WhoAmIResponse response = (WhoAmIResponse)svc.Execute(request);
+
+                    Console.WriteLine("Your UserId is {0}", response.UserId);
+
+                    Console.WriteLine("Press any key to exit.");
+                }
+                else Console.WriteLine("There is a problem connecting to D365.");
+                Console.ReadLine();
+            }
         }
     }
 }
